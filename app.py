3f0bfd0c90b70cb80cb6112006aa9568df13e67b
Kenneth Liau <liauken@gmail.com>
2013-01-12 01:08:17 -0500

    added db info, redirect, 404

--- app.py
+++ app.py
@@ -11,11 +11,8 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 # configuration
 DATABASE = '/tmp/kenly.db'
-#DEBUG = True
-SECRET_KEY = 'u5\xfa&Hm|\xe7\x8f\xa2\x9cH\xe62K\x18\xb1X\xf1\xd7\x04\xa8\xf6\x9c'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
+url_dict = {}
+hotness_dict = {}
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -23,7 +20,6 @@ app.config.from_object(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
-
 
 @app.route("/result/", methods=['POST'])
 def add_urls():
@@ -40,15 +36,73 @@ def add_urls():
         result = "The URL is not valid! Make sure it has valid protocol and domain name."
         return render_template('result.html', result=result)
 
-    
+    #url is valid. Adding to database
+    if user_url in url_dict:
+        h = hotness_dict[user_url]
+        print h
+        h = h + 1
+        hotness_dict[user_url] = h
+        print hotness_dict[user_url]
+        random_chars = url_dict[user_url]
+        redirect_url = request.url_root + 'url/' + random_chars
 
-    
-    return render_template('result.html', result=result, url=user_url)
+    else:
+        random_chars = random_characters()
+        while random_chars in url_dict.values():
+            print 'random chars existed, recreate'
+            random_chars = random_characters()
+        url_dict[user_url] = random_chars
+        hotness_dict[user_url] = 1
+        redirect_url = request.url_root + 'url/' + random_chars
 
-@app.route("/url/<string:id>/")
-def redirection(id):
-    return render_template('redirection.html')
+    return render_template('result.html', result=result, url=user_url, redirect_url=redirect_url)
 
+@app.route("/url/<string:redirect_id>/")
+def redirection(redirect_id):
+    print 'id is ' + redirect_id
+
+    if redirect_id in url_dict.values():
+        redirect_url = find_key(url_dict, redirect_id)
+        print 're_url is ' + redirect_url
+        return redirect(redirect_url)
+
+    else:
+        abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html'), 404
+
+@app.errorhandler(405)
+def page_not_found(error):
+    return render_template('error.html'), 405
+
+
+def find_key(dic, val):
+    return [k for k, v in dic.iteritems() if v == val][0]
+
+
+def random_characters():
+    n = random.choice('1234567')
+    return ''.join(random.choice(string.letters) for i in range(int(n)))
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    if hasattr(g, 'db'):
+        g.db.close()
 
 def get_server_status_code(url):
     """
@@ -73,28 +127,6 @@ def check_url(url):
     good_codes = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
     return get_server_status_code(url) in good_codes
 
-
-
-def random_characters():
-    return ''.join(random.choice(string.letters) for i in range(5))
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-@app.teardown_request
-def teardown_request(exception):
-    if hasattr(g, 'db'):
-        g.db.close()
 
 if __name__ == "__main__":
   # Bind to PORT if defined, otherwise default to 5000.
